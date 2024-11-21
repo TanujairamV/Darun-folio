03fcc93e675ed4ea24bf11607b54ce766fb15015
Darun-S <tanujairam.edu@gmail.com>
2024-11-21 21:54:03 +0530

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Student",
+          "Python beginner",
         ],
         autoStart: true,
         loop: true,
